e34cdf44bd0e276696d479edce2570d03257e96d
tiberio_s_ferreira <tiberio.ferreiracs@gmail.com>
2025-11-27 17:05:07 -0300

    feat(cpf): adiciona função de limpeza de caracteres e transforma em números

--- lib/brazilian_docs/cpf.rb
+++ lib/brazilian_docs/cpf.rb
@@ -1,5 +1,13 @@
 module BrazilianDocs 
   class CPF
+    
+    def initialize(doc)
+      # Remove tudo que não for número do CPF.
+      @document = doc.to_s.gsub(/[^0-9]/, '')
+      # Converte cada caractere para número.
+      @digits = @document.chars.map(&:to_i)
+    end
+
     def self.valid?(value)
       # implementar depois
       false
