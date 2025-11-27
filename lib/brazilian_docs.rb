d07d2c7f32202bce7c7bee32d709d4e70e73d0cc
tiberio_s_ferreira <tiberio.ferreiracs@gmail.com>
2025-11-27 15:07:22 -0300

    feat: adiciona as class ao arquivo brazilian_docs.rb

--- lib/brazilian_docs.rb
+++ lib/brazilian_docs.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require_relative "brazilian_docs/version"
+require_relative "brazilian_docs/cpf"
+require_relative "brazilian_docs/cnpj"
 
 module BrazilianDocs
   class Error < StandardError; end
