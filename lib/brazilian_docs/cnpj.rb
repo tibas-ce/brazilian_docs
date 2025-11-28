df12dd26f7d131960d012517a78488d04f242012
tiberio_s_ferreira <tiberio.ferreiracs@gmail.com>
2025-11-28 17:06:18 -0300

    feat(cnpj): adiciona função de limpeza e transforma em números

--- lib/brazilian_docs/cnpj.rb
+++ lib/brazilian_docs/cnpj.rb
@@ -1,5 +1,13 @@
 module BrazilianDocs 
   class CNPJ
+
+    def initialize(doc)
+      # Remove tudo que não for número do CNPJ.
+      @document = doc.to_s.gsub(/[^0-9]/, '')
+      # Converte cada caractere para número.
+      @digits = document.chars.map(&:to_i)
+    end
+
     def self.valid?(value)
       # implementar depois
       false
